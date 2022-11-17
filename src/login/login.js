077b56a8bae0308eea7d88aeee2db5413203d8d5
Andrea dos Santos <andreadossantos@iMac-de-Andrea.local>
2022-11-17 14:26:29 -0300

    ajustes finais

--- src/login/login.js
+++ src/login/login.js
@@ -5,39 +5,39 @@ export default () => {
   const container = document.createElement('div');
   container.classList.add('container-login');
   const template = `
-    <section class='box-login'>
-        <div class='box-content'>
-          <header class='container-top-logo'>
-            <img src='./images/logo_02_blue_081E26.png' id='logo-login' alt='logo do título'>
-          </header>
-          <div class='container-top-subtitle'>
-              <p id="sub-title">MOMENTOS MUSICAIS INESQUECÍVEIS</p>
-          </div>
-          <form class='form-login'>           
-            <input class='box-email' id='email-user' type='email' placeholder='Digite seu e-mail'/>                      
-            <input class='box-password' id='password-user' type='password' placeholder='Digite sua senha'/>
-            <button class='btn-enter' type='submit' id='btn-submit'>Entrar</button>
-            <a href='#password' id='btn-password'>
-              <button class='new-password' type='button'>Esqueci a senha</button>
-            </a>  
-            <button class='box-login-google' type='button' id='btn-google'><img class='logo-google' src='./images/simbolo-do-google.png'/>Login com Google</button>
-            <button class='box-register-login' type='button' id='btn-register'><a href='#register'id='new-user'>Cadastre-se</a></button> 
-          </form>
-          <footer>
-            <p class='footer-text'>Desenvolvido por
-              <a href='https://github.com/AngelMelo12'>
-                  <span class='name'> Angelica Melo, </span>
-              </a>
-              <a href='https://github.com/Canzua'>
-                  <span class="name"> Andrea dos Santos e </span>
-              </a>   
-              <a href='https://github.com/ClareanaRibeiro'>
-                  <span class="name"> Clareana Ribeiro </span>
-              </a>
-            </p>    
-          </footer>
+    <div class='box-login'>
+      <section class='box-content'>
+        <header class='container-top-logo'>
+          <img src='./images/logo_02_blue_081E26.png' id='logo-login' alt='logo do título'>
+        </header>
+        <div class='container-top-subtitle'>
+            <p id="sub-title">MOMENTOS MUSICAIS INESQUECÍVEIS</p>
         </div>
+        <form class='form-login'>           
+          <input class='box-email' id='email-user' type='email' placeholder='Digite seu e-mail'/>                      
+          <input class='box-password' id='password-user' type='password' placeholder='Digite sua senha'/>
+          <button class='btn-enter' type='submit' id='btn-submit'>Entrar</button>
+          <a href='#password' id='forgot-password'>
+            <button class='box-new-password' type='button'>Esqueci a senha</button>
+          </a>  
+          <button class='box-login-google' type='button' id='btn-google'><img class='logo-google' src='./images/simbolo-do-google.png'/>Login com Google</button>
+          <button class='box-register-login' type='button' id='btn-register'><a href='#register'id='new-user'>Cadastre-se</a></button> 
+        </form>
+        <footer>
+          <p class='footer-text'>Desenvolvido por
+            <a href='https://github.com/AngelMelo12'>
+                <span class='name'> Angelica Melo, </span>
+            </a>
+            <a href='https://github.com/Canzua'>
+                <span class="name"> Andrea dos Santos e </span>
+            </a>   
+            <a href='https://github.com/ClareanaRibeiro'>
+                <span class="name"> Clareana Ribeiro </span>
+            </a>
+          </p>    
+        </footer>
     </section>
+    </div>
   `;
 
   container.innerHTML = template;
